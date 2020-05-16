5587d9e9183ac646cccb8c75acdd4dbacf93d2e8
preemek088 <przemasiek0908@gmail.com>
2020-05-16 15:05:19 +0200

    first commit

--- Game/run.py
+++ Game/run.py
@@ -21,8 +21,9 @@ class Player:
         self.left = False
         self.right = False
         self.standing = True
+        self.climbing = False
         self.walk_position = 0
-        self.velocity = 20
+        self.velocity = 10
         self.hitbox = (self.x + 10, self.y +70 , 160, 180)
 
     def draw(self, screen):
@@ -36,43 +37,35 @@ class Player:
                 screen.blit(robo_walking[0], (self.x, self.y))
                 self.walk_position = 1
 
-            # if self.walkCount == 0:
-            #     screen.blit(robo_walking[0], (self.x, self.y))
-            #     self.walkCount += 1
-            # elif self.walkCount == 1:
-            #     screen.blit(robo_walking[1], (self.x, self.y))
-            #     self.walkCount += 1
-            # elif self.walkCount == 2:
-            #     screen.blit(robo_walking[2], (self.x, self.y))
-            #     self.walkCount +=1
-            # elif self.walkCount == 3:
-            #     screen.blit(robo_walking[3], (self.x, self.y))
-            #     self.walkCount +=1
-            # elif self.walkCount == 4:
-            #     screen.blit(robo_walking[4], (self.x, self.y))
-            #     self.walkCount +=1
-            # elif self.walkCount == 5:
-            #     screen.blit(robo_walking[5], (self.x, self.y))
-            #     self.walkCount =0
         self.hitbox = (self.x + 10, self.y + 70, 160, 180)
         pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
 
 
 class Object:
+
     def __init__(self, x, y, height, width):
         self.x = x
         self.y = y
         self.height = height
         self.width = width
-        self.hitbox = (self.x + 5, self.y +40 , 160, 180)
+        self.hitbox = (self.x + 5, self.y +40 , 70, 70)
 
     def draw(self, screen):
 
         screen.blit(ladder_img, (self.x, self.y))
-        screen.blit(ladder_img, (self.x, self.y+50))
-        screen.blit(ladder_img, (self.x, self.y + 100))
+        self.hitbox = (self.x, self.y, 70, 70)
+        pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
 
-        self.hitbox = (self.x + 5, self.y +40 , 160, 180)
+        screen.blit(ladder_img, (self.x, self.y+50))
+        self.hitbox = (self.x, self.y + 50, 70, 70)
+        pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
+
+        screen.blit(ladder_img, (self.x, self.y + 100))
+        self.hitbox = (self.x, self.y + 100, 70, 70)
+        pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
+
+        screen.blit(ladder_img, (self.x, self.y + 150))
+        self.hitbox = (self.x, self.y + 150, 70, 70)
         pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
 
 
@@ -92,6 +85,8 @@ running = True
 robot = Player(600,550,2,2)
 ladder = Object(100,550,2,2)
 
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -99,16 +94,38 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_RIGHT] and robot.x + 170 < 1300:
+    def is_climbing():
+        if robot.x + 40 == ladder.x and robot.y < 550:
+            robot.climbing = True
+        else:
+            robot.climbing = False
+
+    is_climbing()
+
+    if keys[pygame.K_RIGHT] and robot.x + 170 < 1300 and robot.climbing is False:
         robot.x += robot.velocity
         robot.standing = False
-    elif keys[pygame.K_LEFT] and robot.x - 10 > 0:
+        print("Pozycja robota x :{}".format(robot.x))
+        print("Pozycja drabiny x :{}".format(ladder.x))
+
+    elif keys[pygame.K_LEFT] and robot.x - 10 > 0 and robot.climbing is False:
         robot.x -= robot.velocity
         robot.standing = False
+        print("Pozycja robota x :{}".format(robot.x))
+        print("Pozycja drabiny x :{}".format(ladder.x))
+        print("Pozycja robota y :{}".format(robot.y))
+
+    elif keys[pygame.K_UP] and robot.x + 60 == ladder.x and robot.y + 240 > ladder.y:
+        robot.y -= robot.velocity
+        robot.climbing = True
+        print("Pozycja robota y :{}".format(robot.y))
+        print("Pozycja drabiny y :{}".format(ladder.y))
     else:
         robot.standing = True
 
+
+
     update_all()
-    print("taktowanie")
+
 
 
